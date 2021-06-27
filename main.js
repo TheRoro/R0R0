2225d717ad0aaef492b326d1415dba7665421921
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-06-27 13:47:02 -0500

    🔒 Replaced and Removed Token
    Regenerated Access token for bot and replaced old token using environment variables.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-
+require("dotenv").config()
 const client = new Discord.Client()
 
 const prefix = '$'
@@ -10,7 +10,7 @@ client.commands = new Discord.Collection()
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
 
-for(const file of commandFiles) {
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     client.commands.set(command.name, command)
 }
@@ -20,35 +20,34 @@ client.once('ready', () => {
 })
 
 client.on('message', async message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
         return
     }
     const args = message.content.slice(prefix.length).split(/ +/)
     const command = args.shift().toLowerCase()
-    if(command === 'joke' || command === 'jokes') {
+    if (command === 'joke' || command === 'jokes') {
         await client.commands.get('joke').execute(message, args)
     }
-    else if(command === 'github') {
+    else if (command === 'github') {
         await client.commands.get('github').execute(message, args)
     }
-    else if(command === "cat" || command === "neko" || command === "gato" || command === "🐈") {
+    else if (command === "cat" || command === "neko" || command === "gato" || command === "🐈") {
         await client.commands.get('cat').execute(message, args)
     }
-    else if(command === "rick" || command === "morty" || command === "geez") {
+    else if (command === "rick" || command === "morty" || command === "geez") {
         await client.commands.get('rick_and_morty').execute(message, args)
     }
-    else if(command === "pokemon" || command === "poke" || command === "pkmn" || command === "pk") {
+    else if (command === "pokemon" || command === "poke" || command === "pkmn" || command === "pk") {
         await client.commands.get('pokemon').execute(message, args)
     }
-    else if(command === "command" || command === "commands" || command === "commandList" || command === "commandList") {
+    else if (command === "command" || command === "commands" || command === "commandList" || command === "commandList") {
         client.commands.get('commands').execute(message, args)
     }
-    else if(command === "drink" || command === "cocktail"){
+    else if (command === "drink" || command === "cocktail") {
         client.commands.get('drink').execute(message, args)
-    } 
-    else if(command === "biti"){
+    }
+    else if (command === "biti") {
         client.commands.get('biti').execute(message, args)
     }
 })
-
-client.login('ODQyOTY5NDIyMjM0NjQ4NTk4.YJ9Ccw.hfReGXxnPi-8DEN_A9Gfygr0bLA')
+client.login(process.env.TOKEN)
