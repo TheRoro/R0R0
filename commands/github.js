5cc89b1b07d5edf90a28dc280ea6dbc2c1bd559d
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-07-31 11:08:10 -0500

    🐛Fix GitHub User not found
    🐛Fix GitHub User not found when the user types a non-existing user or leaves the args empty.

--- commands/github.js
+++ commands/github.js
@@ -5,46 +5,55 @@ module.exports = {
     name: 'github',
     description: 'github user info',
     async execute(message, args) {
-        if(args) {
+        if (args.length > 0) {
             username = args[0]
             let getUserInfo = async () => {
-                let response = await axios.get(`https://api.github.com/users/${username}`)
-                return response.data
+                try {
+                    let response = await axios.get(`https://api.github.com/users/${username}`)
+                    return response.data
+                }
+                catch (err) {
+                    message.channel.send(`Username not found`)
+                    return
+                }
             }
             let userInfo = await getUserInfo()
+            if (!userInfo) {
+                return
+            }
             const Embed = new Discord.MessageEmbed()
-            .setColor('#fafafa')
-            .setTitle(`${userInfo.login}`)
-            .setURL(`${userInfo.html_url}`)
-            .setAuthor(userInfo.name ? `${userInfo.name}` : `${userInfo.login}`, `https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png`, userInfo.blog ? `https://${userInfo.blog}` : `${userInfo.html_url}`)
-            .setDescription(``)
-            .setThumbnail(`${userInfo.avatar_url}`)
+                .setColor('#fafafa')
+                .setTitle(`${userInfo.login}`)
+                .setURL(`${userInfo.html_url}`)
+                .setAuthor(userInfo.name ? `${userInfo.name}` : `${userInfo.login}`, `https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png`, userInfo.blog ? `https://${userInfo.blog}` : `${userInfo.html_url}`)
+                .setDescription(``)
+                .setThumbnail(`${userInfo.avatar_url}`)
 
-            if(userInfo.bio) {
+            if (userInfo.bio) {
                 Embed.addField(`Bio`, `${userInfo.bio}`)
             }
-            if(userInfo.location) {
+            if (userInfo.location) {
                 Embed.addField(`Location`, `${userInfo.location}`)
             }
-            if(userInfo.company) {
+            if (userInfo.company) {
                 Embed.addField(`Company`, `${userInfo.company}`)
             }
-            if(userInfo.email) {
+            if (userInfo.email) {
                 Embed.addField(`E-mail`, `${userInfo.email}`)
             }
-            if(userInfo.blog) {
+            if (userInfo.blog) {
                 Embed.addField(`Web`, `${userInfo.blog}`)
             }
 
             Embed
-            .setImage(`${userInfo.avatar_url}`)
-            .setTimestamp()
-            .setFooter('Data from GitHub API', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
+                .setImage(`${userInfo.avatar_url}`)
+                .setTimestamp()
+                .setFooter('Data from GitHub API', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
 
             message.channel.send(Embed);
         }
         else {
-            message.channel.send(`Invalid arguments :(`)
+            message.channel.send(`Username not found`)
         }
     }
 }
