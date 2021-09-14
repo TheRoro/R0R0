4fc2c5f869231ec75efbbc14f133e04df5ebd4c2
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-09-14 10:21:01 -0500

    ⚡️Add More phrases

--- commands/biti.js
+++ commands/biti.js
@@ -1,15 +1,70 @@
 module.exports = {
-    name: 'biti',
-    description: 'biti',
-    async execute(message, args) {
-        let answers = ['BITI', 'BT', 'BEETEE', 'The Beetees', 'PITI', '????', 'þiti', '🐝☕', 'biti', 'FRICCIÓN', 'piti', ':b: :regional_indicator_t: ', ':eggplant: ', 'BITI S', 'Rafitipiti', 'BINGA', 'beteh', ':nauseated_face: ', ':clown: ', 'BITI!!!!!!!!', 'BEEEEETEEEEEHHH!!!!!', 'AAAAAAAAA', 'lgBT', 'biti biti biti biti biti', 'NO', 'YA ME HARTÉ', 'F', 'merme', 'biticleta', 'biti biti bamba', 'rafitipinga', 'https://avatars.githubusercontent.com/u/50728246?v=4', 'Internal Server Error :woozy_face: ', 'Bitiquin', 'ADIÓS', 'NOOOOOOOOO', 'Seeeeeh']
-        const getId = (min, max) => {
-            return Math.floor(Math.random() * (max - min)) + min
-        }
-        const getAnswer = () => {
-            return answers[getId(0, answers.length - 1)]
-        }
-        let answer =  getAnswer()
-        message.channel.send(answer)
-    }
+	name: 'biti',
+	description: 'biti',
+	async execute(message, args) {
+		let answers = [
+			'BITI',
+			'BT',
+			'BEETEE',
+			'The Beetees',
+			'PITI',
+			'????',
+			'þiti',
+			'🐝☕',
+			'biti',
+			'FRICCIÓN',
+			'piti',
+			':b: :regional_indicator_t: ',
+			':eggplant: ',
+			'BITI S',
+			'Rafitipiti',
+			'BINGA',
+			'beteh',
+			':nauseated_face: ',
+			':clown: ',
+			'BITI!!!!!!!!',
+			'BEEEEETEEEEEHHH!!!!!',
+			'AAAAAAAAA',
+			'lgBT',
+			'pucha mano',
+			'Twitch: Rafitipiti',
+			'BoyScaut',
+			'https://www.twitch.tv/rafitipiti',
+			'rafiti pipipipipi',
+			'pucha biti',
+			'aprende rapido',
+			'bilaron',
+			'no soy vilaron',
+			'pinga',
+			'leyva mongol',
+			'chupapinga',
+			'pi pi pi pi pinga',
+			'biti biti biti biti biti',
+			'NO',
+			'YA ME HARTÉ',
+			'F',
+			'merme',
+			'biticleta',
+			'biti biti bamba',
+			'rafitipinga',
+			'https://avatars.githubusercontent.com/u/50728246?v=4',
+			'https://avatars.githubusercontent.com/u/50728246?v=4',
+			'https://avatars.githubusercontent.com/u/50728246?v=4',
+			'Internal Server Error :woozy_face: ',
+			'Bitiquin',
+			'ADIÓS',
+			'NOOOOOOOOO',
+			'Seeeeeh',
+			'...',
+			'Completo Italiano',
+		]
+		const getId = (min, max) => {
+			return Math.floor(Math.random() * (max - min)) + min
+		}
+		const getAnswer = () => {
+			return answers[getId(0, answers.length - 1)]
+		}
+		let answer = getAnswer()
+		message.channel.send(answer)
+	},
 }
